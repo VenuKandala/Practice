c07b51da7d2b3c36d20287a740bf5cee91422340
rohit <rkzila1234@outlook.com>
2018-11-19 16:28:42 +0530

    First Commit

---
+++ src/Terst.java
@@ -0,0 +1,11 @@
+
+public class Terst {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println(Double.parseDouble("10000000000000000000000"));
+		System.out.println(String.format ("%.2f",  Double.parseDouble("10000000000000000000000")));
+	}
+
+}
